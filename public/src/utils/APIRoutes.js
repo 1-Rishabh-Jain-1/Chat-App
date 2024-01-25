a9526ab6975f883e80a03842b84417dc874dc522
Rishabh <rarishabhjain@gmail.com>
2024-01-25 22:37:53 +0530

    Fixing CORS 4

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chat-app-be-orpin.vercel.app";
+export const host = "https://chat-app-7npvtjn4j-1-rishabh-jain-1s-projects.vercel.app/";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
