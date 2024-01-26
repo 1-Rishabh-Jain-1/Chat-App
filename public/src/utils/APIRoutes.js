8b74b33721256bd38ea24908b5db14ae91d45224
Rishabh <rarishabhjain@gmail.com>
2024-01-26 09:11:45 +0530

    Fixing CORS 10

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,7 +1,7 @@
-export const host = "https://chat-app-be-1-rishabh-jain-1s-projects.vercel.app/";
-export const registerRoute = `${host}api/auth/register`;
-export const loginRoute = `${host}api/auth/login`;
-export const setAvatarRoute = `${host}api/auth/setAvatar`;
-export const allUsersRoute = `${host}api/auth/allUsers`;
-export const sendMessageRoute = `${host}api/messages/addmsg`;
-export const getAllMessagesRoute = `${host}api/messages/getmsg`;
+export const host = "https://chat-app-be-1-rishabh-jain-1s-projects.vercel.app";
+export const registerRoute = `${host}/api/auth/register`;
+export const loginRoute = `${host}/api/auth/login`;
+export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+export const allUsersRoute = `${host}/api/auth/allUsers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
